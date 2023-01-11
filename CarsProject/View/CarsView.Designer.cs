192519097b263a722b7c1d60935e03f063eba293
it.pgee.burgas <it.pgee.burgas@gmail.com>
2023-01-11 10:12:38 +0200

    Add project files.

---
+++ CarsProject/View/CarsView.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace CarsProject.View
+{
+    partial class CarsView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.carsDBDataSet = new CarsProject.CarsDBDataSet();
+            this.carsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.carsTableAdapter = new CarsProject.CarsDBDataSetTableAdapters.CarsTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.carsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sportsCarsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.superCarsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carsDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.carsDataGridViewTextBoxColumn,
+            this.sportsCarsDataGridViewTextBoxColumn,
+            this.superCarsDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.carsBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(442, 135);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // carsDBDataSet
+            // 
+            this.carsDBDataSet.DataSetName = "CarsDBDataSet";
+            this.carsDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // carsBindingSource
+            // 
+            this.carsBindingSource.DataMember = "Cars";
+            this.carsBindingSource.DataSource = this.carsDBDataSet;
+            // 
+            // carsTableAdapter
+            // 
+            this.carsTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // carsDataGridViewTextBoxColumn
+            // 
+            this.carsDataGridViewTextBoxColumn.DataPropertyName = "Cars";
+            this.carsDataGridViewTextBoxColumn.HeaderText = "Cars";
+            this.carsDataGridViewTextBoxColumn.Name = "carsDataGridViewTextBoxColumn";
+            // 
+            // sportsCarsDataGridViewTextBoxColumn
+            // 
+            this.sportsCarsDataGridViewTextBoxColumn.DataPropertyName = "Sports Cars";
+            this.sportsCarsDataGridViewTextBoxColumn.HeaderText = "Sports Cars";
+            this.sportsCarsDataGridViewTextBoxColumn.Name = "sportsCarsDataGridViewTextBoxColumn";
+            // 
+            // superCarsDataGridViewTextBoxColumn
+            // 
+            this.superCarsDataGridViewTextBoxColumn.DataPropertyName = "Super Cars";
+            this.superCarsDataGridViewTextBoxColumn.HeaderText = "Super Cars";
+            this.superCarsDataGridViewTextBoxColumn.Name = "superCarsDataGridViewTextBoxColumn";
+            // 
+            // CarsView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CarsProject.Properties.Resources.thumb_1920_1037084;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "CarsView";
+            this.Text = "CarsView";
+            this.Load += new System.EventHandler(this.CarsView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carsDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private CarsDBDataSet carsDBDataSet;
+        private System.Windows.Forms.BindingSource carsBindingSource;
+        private CarsDBDataSetTableAdapters.CarsTableAdapter carsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn carsDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sportsCarsDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn superCarsDataGridViewTextBoxColumn;
+    }
+}
---
+++ CarsProject/View/CarsView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarsProject.View
+{
+    public partial class CarsView : Form
+    {
+        public CarsView()
+        {
+            InitializeComponent();
+        }
+
+        private void CarsView_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'carsDBDataSet.Cars' table. You can move, or remove it, as needed.
+            this.carsTableAdapter.Fill(this.carsDBDataSet.Cars);
+
+        }
+    }
+}
